fd8540a74fe5063eb612b9830e7de6eddf9744fc
Ronald Pereira <ronald.pereira@sensedia.com>
2025-08-15 16:28:28 -0300

    classe de Exception que nao foi comitada

---
+++ src/main/java/br/com/eventmanager/adapter/inbound/rest/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package br.com.eventmanager.adapter.inbound.rest.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
